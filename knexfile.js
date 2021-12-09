a9006e01222744c628bf6b295499223126008bbf
WaylayOvercast <timelyovercast@gmail.com>
2021-12-09 17:22:25 -0600

    incomplete first commit

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.db4'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+  
+
+  
+
+};
